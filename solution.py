98c1e156f195102319058e62729051365d36b33f
alexp799 <80836235+alexp799@users.noreply.github.com>
2023-04-13 17:05:13 +0300

    sol-2

--- solution.py
+++ solution.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import anderson_ksamp
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 753487228 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    statistic, critical_values, pvalue = anderson_ksamp([x, y])
+    p = 0.05
+    if statistic > critical_values[2]:
+        return True
+    else:
+        return False
